0484c99c7b86977317b3e3136e4bdb9f0101f0fb
Amin Charoliya <amincharoliya@gmail.com>
2020-04-02 16:05:56 +0530

    redirected to worldmeter

--- src/App.js
+++ src/App.js
@@ -123,8 +123,9 @@ class App extends React.Component {
 
   componentDidMount() {
 
-    this.getCountryDataOnLoad('IN');
-    this.getAllData();
+    //this.getCountryDataOnLoad('IN');
+    //this.getAllData();
+	window.location.href = "https://www.worldometers.info/coronavirus/";
 
   }
   
@@ -159,95 +160,9 @@ class App extends React.Component {
         </div>
 
 
-        <section className="App-header w-full max-w-screen-xl relative mx-auto px-6" style={{minHeight: '100vh', alignContent: 'center', display: 'flex', flexDirection: 'column', justifyContent: 'center'}}>
+       <section class="App-header w-full max-w-screen-xl relative mx-auto px-6" style="min-height: 100vh; place-content: center; display: flex; flex-direction: column;">Data website is down, redirecting to <a href="https://www.worldometers.info/coronavirus/"> https://www.worldometers.info/coronavirus/ </a></section>
 
-          <div className="data-wrap" style={{display: 'flex'}}>
-
-            <div className="global-section mb-3">
-
-              <h2 className="font-bold text-center mb-3 text-3xl">Global</h2>
-
-              <table class="table-auto w-full text-center mb-4 text-xl">
-                <thead>
-                  <tr>
-                    <th class="px-4 py-2 text-blue-500">Total Cases</th>
-                    <th class="px-4 py-2 text-red-500">Total Deaths</th>
-                    <th class="px-4 py-2 text-green-500">Total Recovered</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td class="border px-4 py-2 text-blue-500 font-semibold">{this.state.globalTotal_cases}</td>
-                    <td class="border px-4 py-2 text-red-500 font-semibold">{this.state.globalTotal_deaths}</td>
-                    <td class="border px-4 py-2 text-green-500 font-semibold">{this.state.globalTotal_recovered}</td>
-                  </tr>
-                </tbody>
-              </table>
-
-            </div>
-
-
-            <div className="text-center country-section">
-              <h2 className="text-xl font-bold text-center mb-4 text-2xl">{this.state.data["country"]}</h2>
-
-              <table class="table-auto mb-4 text-left inline-block country-table text-lg">
-                <tbody>
-                  <tr>
-                    <th class="px-4 py-2">Cases Today</th>
-                    <td class="border px-4 py-2 text-center font-semibold">{ this.state.data["CountryTodayCases"] }</td>
-                  </tr>
-                  <tr>
-                    <th class="px-4 py-2 text-red-600">Deaths Today</th>
-                    <td class="border px-4 py-2  text-center text-red-600 font-semibold">{ this.state.data["CountryTodayDeaths"] }</td>
-                  </tr>
-                  <tr>
-                  <th class="px-4 py-2 text-blue-700">Total Active Cases</th>
-                    <td class="border px-4 py-2 text-center font-semibold text-blue-700">{ this.state.data["countryTotal_active_cases"] }</td>
-                  </tr>
-                </tbody>
-              </table>
-
-              <table class="table-auto mb-4 text-left inline-block country-table ">
-                <tbody>
-                  <tr>
-                    <th class="px-4 py-2 text-blue-500">Total Cases</th>
-                    <td class="border px-4 py-2 text-center text-blue-500 font-semibold">{ this.state.data["countryTotal_cases"] }</td>
-                  </tr>
-                  <tr>
-                    <th class="px-4 py-2 text-red-500">Total Deaths</th>
-                    <td class="border px-4 py-2  text-center text-red-500 font-semibold">{ this.state.data["countryTotal_deaths"] }</td>
-                  </tr>
-                  <tr>
-                  <th class="px-4 py-2 text-green-500">Total Cases Recovered</th>
-                    <td class="border px-4 py-2  text-center text-green-500 font-semibold">{ this.state.data["countryTotal_recovered"] }</td>
-                  </tr>
-                </tbody>
-              </table>
-
-            </div>
-
-          </div>
-
-
-          <div className="text-center color-black-300 mt-8 mb-12">
-            <div class="relative w-2/4 inline-block">
-              <select className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" onChange={(event) => this.getCountryData(event.target)}>
-                <option value="IN">India</option>
-                { countriesList }
-              </select>
-              <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700"><svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"></path></svg></div>
-              {this.state.isLoading ? <Loader /> : ''}
-            </div>
-          </div>
-
-        </section>
-
-        <footer className="footer z-100">
-          <div className="m-h-16 px-4 py-2 max-w-screen-xl mx-auto">
-            <p className="float-left">Developed by <a href="https://www.amincharoliya.com" className="text-blue-600">Amin Charoliya</a></p>
-            <p className="float-right">Data used from <a className="text-blue-600" href="https://thevirustracker.com/" target="_blank">thevirustracker.com</a></p>
-          </div>
-      </footer>
+        
 
       </div>
 
